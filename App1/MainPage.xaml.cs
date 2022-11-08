931b5ff151aa0d15e113d283be9a38f330fd3a58
Aidan Doherty <aidan2129@gmail.com>
2022-11-08 23:50:47 +0000

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Automation; 
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            List<Item> items = new List<Item>
+            {
+               new Item { Id=1, Name="Ball", Description="Hello" },
+               new Item { Id=2, Name="Hat", Description="Test" }
+            };
+            this.InitializeComponent();
+            for (int i = 0; i < List.Lenght, i++) {
+                ListBoxName.Items.Add(MyObjectList[i]);
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
